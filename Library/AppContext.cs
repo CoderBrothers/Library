3808d55ba5ccddf5334f3040cf67437e4af6e199
CoderBrothers <121336523+CoderBrothers@users.noreply.github.com>
2024-01-02 11:28:11 +0200

    Added sqlite

---
+++ Library/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryApp
+{
+    //Две колекции типа Dbset, для читателей и для книг
+    public class AppContext : DbContext
+    {
+
+    }
+}
